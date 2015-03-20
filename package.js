ba11df6924e003d034f9ddf0ef4b7dfe8a6de11e
Jonathan Perl <perl.jonathan@gmail.com>
2015-03-20 18:54:19 -0400

    Bump and release

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dispatch:platform',
   summary: 'Platform detection and normalization.',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/DispatchMe/meteor-platform.git'
 });
 
