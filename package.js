5144a8611094da62d747ce01b4fb6980154531a5
Jonathan Perl <perl.jonathan@gmail.com>
2015-09-14 18:47:01 -0400

    Update cordova plugins
    Waiting until 1.2 is released to publish

--- package.js
+++ package.js
@@ -1,27 +1,22 @@
 Package.describe({
   name: 'dispatch:platform',
   summary: 'Platform detection and normalization.',
-  version: '1.0.2',
+  version: '2.0.0',
   git: 'https://github.com/DispatchMe/meteor-platform.git'
 });
 
 Cordova.depends({
-  'com.cordova.plugins.sms': 'https://github.com' +
-  '/DispatchMe/cordova-sms-plugin' +
-  '/tarball/f19357dd17135af686dbf9fb782ee7fe01b86a2d',
+  'com.cordova.plugins.sms': 'hhttps://github.com/cordova-sms/cordova-sms-plugin.git#17c4da078f6c0d9f762ac10f5015440ee1c81d07',
 
-  'de.appplant.cordova.plugin.email-composer': 'https://github.com' +
-  '/DispatchMe/cordova-plugin-email-composer' +
-  '/tarball/e0466a2bedf6798228ce5e46db21481e665641e1',
+  'de.appplant.cordova.plugin.email-composer': 'https://github.com/katzer/cordova-plugin-email-composer.git#db9c3aa9a7923c427bb85fea82f4e95f445a28f3',
 
   // This prevents the annoying Undo Edit popup on iOS.
-  'fr.spirotron.cordova.ios.disableshaketoedit': 'https://github.com' +
-  '/nleclerc/cordova-plugin-ios-disableshaketoedit' +
-  '/tarball/685f971fd0328fd167ea443f5638cd85e3fc035f'
+  'fr.spirotron.cordova.ios.disableshaketoedit': 'https://github.com/DispatchMe/cordova-plugin-ios-disableshaketoedit.git#685f971fd0328fd167ea443f5638cd85e3fc035f'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.0');
+  api.versionsFrom('1.2');
+
   api.use('jquery', 'web');
 
   api.addFiles(['platform.js', 'normalize.js'], 'web');
