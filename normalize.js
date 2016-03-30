8f7e9e91e242d744b2244aa39dd22826cc7401d0
Derek Caneja <derek@dispatch.me>
2016-03-30 18:14:14 -0400

    Update package to use versions from Meteor 1.3
    Use ES2015 conventions.
    Update cordova plugins to latest versions.

--- normalize.js
+++ normalize.js
@@ -1,18 +1,16 @@
 if (Platform.isIos) {
-  Meteor.startup(function () {
-    // Prevent fixed positions from moving or resizing
-    // on iOS7 whenever an input is blurred.
+  Meteor.startup(() => {
+    // Prevent fixed positions from moving or resizing on iOS7 whenever an input is blurred.
     // http://stackoverflow.com/a/24670746/230462
-    $(document).on('blur', 'input, textarea', function () {
-      setTimeout(function () {
+    $(document).on('blur', 'input, textarea', () => {
+      setTimeout(() => {
         window.scrollTo(document.body.scrollLeft, document.body.scrollTop);
       }, 0);
     });
 
-    //Adds touchstart listener to <body>.
-    //Required for :active pseudo class to work in iOS.
-    //https://developers.google.com/web/fundamentals/input/touch/activestates/index?hl=en
-    document.body.addEventListener('touchstart', function () {
-    }, false);
+    // Adds touchstart listener to <body>.
+    // Required for :active pseudo class to work in iOS.
+    // https://developers.google.com/web/fundamentals/input/touch/activestates/index?hl=en
+    document.body.addEventListener('touchstart', () => {}, false);
   });
 }
